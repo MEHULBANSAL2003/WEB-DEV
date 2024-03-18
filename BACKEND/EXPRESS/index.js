506313571d97a1431f41a4f027ae71cbd50bd9d8
Mehul Bansal <mehulbansal83606@gmail.com>
2024-03-18 14:26:29 +0530

    recieving the request from browser by use method

--- BACKEND/EXPRESS/index.js
+++ BACKEND/EXPRESS/index.js
@@ -10,9 +10,14 @@ const app=express(); // the value is returned is stored in variable app
 // in simple words.. like we connect charger and mobile through physical port to connect them and charge the mobile
 // similarly these ports establish a connection with request and response.. where exchanges takes place
 
-let port= 3000;    // other port is 8080..and many more
+let port= 8080;    // other port is 3000..and many more
 
 
  app.listen(port,()=>{   // listen makes a web server that listens to the incoming api requests
     console.log(`app listening on port ${port}`);
+ })
+
+
+ app.use((req,res)=>{
+    console.log("request recieved");
  })
