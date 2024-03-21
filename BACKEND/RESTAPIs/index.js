d685849bbab029a6a915c8a49adfb2387437f365
Mehul Bansal <mehulbansal83606@gmail.com>
2024-03-21 18:44:39 +0530

    creating get request for all the post -> index route

--- BACKEND/RESTAPIs/index.js
+++ BACKEND/RESTAPIs/index.js
@@ -10,15 +10,48 @@ app.use(express.urlencoded({extended:true}));
 app.set("view engine","views");
 app.set("views",path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 
 
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`);
 })
 
-app.get("/",(req,res)=>{
+
+
+// for now we dont have data stored in our data base.. 
+// to replicare that... we will create our array and store there for now
+
+let posts=[
+
+{
+    username:"apnacollege",
+    content: "i love coding!!"
+}, 
+
+{
+    username:"mehulbansal",
+    content: "hardwork is important to achieve success"
+},
+{
+    username:"mehul",
+    content: "hardwork always win over talent"
+},
+{
+    username:"rahul",
+    content: "i got selectd for 1st internship"
+}
+
+
+
+];
+
+
+
+app.get("/posts",(req,res)=>{          // making get requests for all the  posts and we call it INDEX ROUTE
     console.log("server running well");
-    res.send("server running well");
+  //  res.send("server running well");
+  res.render("index.ejs",{posts}); 
 })
+
 
