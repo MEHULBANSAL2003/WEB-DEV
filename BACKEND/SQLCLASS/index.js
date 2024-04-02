afca6589c5643a9f87eed303fb813b6fdf190bfc
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-02 17:55:59 +0530

    connecting vscode with mysql workbench

--- BACKEND/SQLCLASS/index.js
+++ BACKEND/SQLCLASS/index.js
@@ -1,5 +1,33 @@
 const {faker}=require("@faker-js/faker");
 
+const mysql=require("mysql2");  // requiring mysql
+
+// creating a connection to database
+ // to create a connection.. we need to create a database first in myysql workbench 
+const connnection=mysql.createConnection({
+host:"localhost",
+user:"root",
+database:'delta_app',  // name of the databse
+password:"Mehul@123"
+});
+
+// using queries
+
+try{
+
+    connnection.query("SHOW TABLES",(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+    })
+
+} catch(err){
+    console.log(err);
+}
+
+connnection.end();  // to end the connection..!!
+
+
+
 let getRandomUser=()=>{
     return {
    
