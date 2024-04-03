6f532a8690e31b3f7648094e5aadd747af18c204
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-03 10:59:31 +0530

    inserting data into database with commands in vscode

--- BACKEND/SQLCLASS/index.js
+++ BACKEND/SQLCLASS/index.js
@@ -26,7 +26,60 @@ try{
     console.log(err);
 }
 
+
+
+
+
+//INSERTING NEW DATA INTO TABLE USER..!!
+// let q="INSERT INTO  user(id,username,email,password) VALUES (?,?,?,?)" ; // these ? are placeholder that are passed dynamically..
+// let user=["123","123_newuser","abc@gmail.com","abc"]; // these are the values to be passed..!! in place of ?
+
+// try{
+
+// connnection.query(q,user,(result,err)=>{  // adding the data through query
+// if(err) throw err;
+// console.log(result);
+// });
+// } catch(err){
+//     console.log(err);
+// }
+
+// INSERTING MULTIPLE DATA IN A GO..!!
+
+
+let q1="INSERT INTO  user(id,username,email,password) VALUES ?" ; // we can just write single ? to enter all the data
+let users=[
+    ["123b","123_newuserb","abc@gmail.comb","abcb"],
+    ["123c","123_newuserc","abc@gmail.comc","abcc"]  
+];
+try{
+
+connnection.query(q1,[users],(result,err)=>{  // users is passes in array bcoz users is 2d array and we want each id array to be inserted
+if(err) throw err;
+console.log(result);
+});
+} catch(err){
+    console.log(err);
+}
+
+
+
+
+
+
 connnection.end();  // to end the connection..!!
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
