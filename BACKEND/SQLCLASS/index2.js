81727cfab49f19fc66cf8b390512901e6a350968
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-04 02:16:26 +0530

    editing the username in 2 steps

--- BACKEND/SQLCLASS/index2.js
+++ BACKEND/SQLCLASS/index2.js
@@ -2,8 +2,15 @@ const express= require("express");
 const {faker}=require("@faker-js/faker");
 const path=require("path"); 
 const mysql=require("mysql2");  // requiring mysql
+const methodOverride=require("method-override");
+// const { Connection } = require("mysql2/typings/mysql/lib/Connection");
+
 
 const app=express();
+
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
 
 const port=8080;
 app.set("view engine","ejs");
@@ -66,4 +73,59 @@ app.get("/user",(req,res)=>{
         console.log(err);
     }
 })
+
+
+// get request to serve form to edit the username
+app.get("/user/:id/edit",(req,res)=>{
+    let {id}=req.params;
+        // now we will search user based on id..!!
+        let q=`SELECT * FROM user where id='${id}'`;  
+
+
+        try{
+            connnection.query(q,(err,result)=>{
+                if(err) throw err;
+                 console.log(result); // result will be in form of array
+                 let user=result[0];  // to extract the data of user
+                
+                res.render("edit.ejs",{user});
+            })
+        }catch(err){
+            console.log(err);
+        }
+
+})
+// patch request to update the data if password entered is correct
+app.patch("/user/:id",(req,res)=>{
+
+    let {password:formPass, username:newUserName}=req.body;
+    let {id}=req.params;
+    // console.log(id);
+    // console.log(pass);
+
+    let q=`SELECT * FROM user WHERE id="${id}"`;
+
+    try{ 
+    connnection.query(q,(err,result)=>{
+      if(err) throw err;
+      let user=result[0];
+
+      if(formPass!=user.password){
+        res.send("WRONG PASSWORD..!!");
+      }
+      else{
+        let q2=`UPDATE user SET username="${newUserName}" WHERE id="${id}"`;
+       connnection.query(q2,(err,result)=>{
+        if(err) throw err;
+        res.send(result);
+       });
+      }
+
+     })
+
+}catch(err){
+    console.log(err);
+}
+
+});
 
