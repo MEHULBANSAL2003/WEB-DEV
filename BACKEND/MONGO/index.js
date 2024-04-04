23c2cbbaad770d30f9f17adfc2a23f77637380a0
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-05 00:47:22 +0530

    deleting documents

--- BACKEND/MONGO/index.js
+++ BACKEND/MONGO/index.js
@@ -34,86 +34,95 @@ const Employee=mongoose.model("Employee",userSchema);  // employees name collect
 
 // making documents
 
-const user1=new User({name:"Adam",email:"adam@yahoo.in",age:48});  // user1 document is created but not saved to db yet
+// const user1=new User({name:"Adam",email:"adam@yahoo.in",age:48});  // user1 document is created but not saved to db yet
 
-const user2=new User({name:"Eve",email:"eve.google.com",age:48});
+// const user2=new User({name:"Eve",email:"eve.google.com",age:48});
 
 
-user1.save()  // to save the record into db..!!  this method returns promise
+// user1.save()  // to save the record into db..!!  this method returns promise
 
-user2.save()
+// user2.save()
+// .then((res)=>{
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// });
+
+// //inserting many at the same time
+
+// User.insertMany([
+
+//   {name:"Tony",email:"tony@gmail.com",age:50},
+//   {name:"Peter",email:"peter@gmail.com",age:30},
+//   {name:"Bruce",email:"bruce@gmail.com",age:47},
+// ]).then((data)=>{
+//   console.log(data);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+
+//  // find method to search for documents
+
+//  User.find({}).then((res)=>{
+//   console.log(res);
+//  }).catch((err)=>{
+//   console.log(err);
+//  });
+
+
+// // find method with some filter
+// User.find({age:{$gt:47}}).then((res)=>{
+//   console.log(res[0].name);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// // special method to find the document by id..!!
+
+// User.findById("660eefd77cc8d774c5515752")
+// .then((res)=>{
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// // updating the document
+
+// User.updateOne({name:"Bruce"}, {age:49}) // this query will search for document with anme bruce and update its age to 49
+// .then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+// // uppdate many...!!
+// User.updateMany({age:{$gt:48}}, {age:55})
+// .then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+// // find and update
+
+// User.findOneAndUpdate({name:"Bruce"},{age:35},{new:true})
+// .then((res)=>{
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+// delete query
+User.deleteOne({name:"Bruce"})
 .then((res)=>{
   console.log(res);
 })
-.catch((err)=>{
-  console.log(err);
-});
-
-//inserting many at the same time
-
-User.insertMany([
-
-  {name:"Tony",email:"tony@gmail.com",age:50},
-  {name:"Peter",email:"peter@gmail.com",age:30},
-  {name:"Bruce",email:"bruce@gmail.com",age:47},
-]).then((data)=>{
-  console.log(data);
-})
-.catch((err)=>{
-  console.log(err);
-})
-
-
- // find method to search for documents
-
- User.find({}).then((res)=>{
-  console.log(res);
- }).catch((err)=>{
-  console.log(err);
- });
-
-
-// find method with some filter
-User.find({age:{$gt:47}}).then((res)=>{
-  console.log(res[0].name);
-})
-.catch((err)=>{
-  console.log(err);
-})
-
-// special method to find the document by id..!!
-
-User.findById("660eefd77cc8d774c5515752")
-.then((res)=>{
-  console.log(res);
-})
-.catch((err)=>{
-  console.log(err);
-})
-
-// updating the document
-
-User.updateOne({name:"Bruce"}, {age:49}) // this query will search for document with anme bruce and update its age to 49
-.then((res)=>{
-  console.log(res);
-}).catch((err)=>{
-  console.log(err);
-})
-
-// uppdate many...!!
-User.updateMany({age:{$gt:48}}, {age:55})
-.then((res)=>{
-  console.log(res);
-}).catch((err)=>{
-  console.log(err);
-})
-
-// find and update
-
-User.findOneAndUpdate({name:"Bruce"},{age:35},{new:true})
-.then((res)=>{
-  console.log(res);
-})
-.catch((err)=>{
+.then((err)=>{
   console.log(err);
 })
