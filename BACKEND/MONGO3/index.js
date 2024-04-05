5b99634294a795444c56d8b926a7caa90e1809cd
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-05 21:53:33 +0530

    creating chat db and storing 1st chat info

--- BACKEND/MONGO3/index.js
+++ BACKEND/MONGO3/index.js
@@ -3,6 +3,7 @@ const app=express();
 const port=8080;
 const mongoose=require("mongoose");
 const path=require("path");
+const Chat=require("./models/chat.js");  // requiring the chat.js file which is exported 
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs"); 
@@ -18,6 +19,21 @@ main()
 async function main(){
 await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+
+let chat1=new Chat({
+    from:"neha",
+     to:"priya",
+     msg:"send me your exam sheets",
+     created_at: new Date()  // UTC format time
+});
+
+chat1.save()
+.then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 
 app.get("/",(req,res)=>{
