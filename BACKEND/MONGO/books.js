8ae3788aca5f3792661ed86e9e28e90854354219
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-05 11:46:01 +0530

    getting familiar with enum parameter

--- BACKEND/MONGO/books.js
+++ BACKEND/MONGO/books.js
@@ -25,11 +25,17 @@ async function main() {
     type:String
    },
    price:{
-    type:Number
+    type:Number,
+    min:1   // this is property of number., that minimum value is 1
    },
    discount:{
     type:Number,
     default:0    // this means discount is set to 0 by default
+   },
+
+   category:{
+    type:String,
+    enum:["fiction", "non-fictuion"], // enum is used to store the data if category is only matches the value in enum array
    }
 
 
@@ -72,6 +78,25 @@ let book2=new Book({
 
 
 book2.save()   // saving document to collection 
+.then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
+// this book will get saved as category is matched to enum
+let book3=new Book({
+    title:"Marvel comic",  // this will fail because maxlength is to be 20
+    price: "499",
+    category:"fiction"
+})
+
+// this syntax is valid as 400 is converted into 400..!!
+
+
+book3.save()   // saving document to collection 
 .then((res)=>{
     console.log(res);
 })
