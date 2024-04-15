aaed44f7add617cb727ead5f466d7fbd093a8ef0
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-16 00:09:54 +0530

    implementing one to many with 2 collections

--- BACKEND/RELATIONSHIPS(MONGODB)/Models/customer.js
+++ BACKEND/RELATIONSHIPS(MONGODB)/Models/customer.js
@@ -34,14 +34,30 @@ const customerSchema=new mongoose.Schema({
 const Order=mongoose.model("Order",orderSchema);
 const Customer=mongoose.model("Customer",customerSchema);
 
+const addCustomer=async()=>{
+    let cust1=new Customer({
+         name:"Rahul Kumar"
+    });
+    // now i will stor the orders for cust1.. first i have to extract the orders for order table..
+    let order1=await Order.findOne({item:"Chips"});
+    let order2=await Order.findOne({item:"Chocolate"});
 
-const addOrders=async()=>{
-    let res=await Order.insertMany([
-       {item:"Samosa",price:12},
-       {item:"Chips",price:10},
-       {item:"Chocoloate",price:40}
-    ]);
-    console.log(res);
+    cust1.orders.push(order1);
+    cust1.orders.push(order2);
+  /// though we pushed order1 and order2 totally.. but in mongodb only there ID is pushed as in schema we defined type to be oBJECTID
+    let result=await cust1.save();
+    console.log(result);
 }
+addCustomer();
 
-addOrders();
+
+// const addOrders=async()=>{
+//     let res=await Order.insertMany([
+//        {item:"Samosa",price:12},
+//        {item:"Chips",price:10},
+//        {item:"Chocolate",price:40}
+//     ]);
+//     console.log(res);
+// }
+
+// addOrders();
