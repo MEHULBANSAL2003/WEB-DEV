7aba21cc9ec112f00a2b548cb6e2a0922a8c1f33
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-15 23:23:54 +0530

    creating one-few realtion in single schema

--- BACKEND/RELATIONSHIPS(MONGODB)/Models/user.js
+++ BACKEND/RELATIONSHIPS(MONGODB)/Models/user.js
@@ -18,8 +18,11 @@ async function main(){
 
 const userSchema=new Schema({
     username:String,
-    adresses:[
-        {
+    // here mongo itself create id for user1 and also for all the address as it takes all the address object as new schema
+        // to stop this , we will explicitly pass _id =false
+    addresses:[
+        {   
+            _id:false,
             location:String,
             city:String,
         }
@@ -27,3 +30,25 @@ const userSchema=new Schema({
 });
 
 const User=mongoose.model("User",userSchema);
+
+const addUser=async()=>{
+    let user1=new User({
+        username:"sherlockholmes",
+        
+        addresses:[{
+            location:"221B Baker Street",
+            city:"London"
+        }]
+    });
+    // anther way of adding address
+    user1.addresses.push({
+
+        location:"P32 WallStreet",
+        city:"London"
+    });
+
+    let result=await user1.save();
+    console.log(result);
+}
+
+addUser();
