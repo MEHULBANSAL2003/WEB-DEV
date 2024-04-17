d091fdfa26d67bc1e010e66d402edea0a81359ff
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-18 00:09:24 +0530

    adding a new customer

--- BACKEND/RELATIONSHIPS(MONGODB)/Models/customer.js
+++ BACKEND/RELATIONSHIPS(MONGODB)/Models/customer.js
@@ -34,26 +34,79 @@ const customerSchema=new mongoose.Schema({
 const Order=mongoose.model("Order",orderSchema);
 const Customer=mongoose.model("Customer",customerSchema);
 
-const addCustomer=async()=>{
-    let cust1=new Customer({
-         name:"Rahul Kumar"
-    });
-    // now i will stor the orders for cust1.. first i have to extract the orders for order table..
-    let order1=await Order.findOne({item:"Chips"});
-    let order2=await Order.findOne({item:"Chocolate"});
+// const addCustomer=async()=>{
+//     let cust1=new Customer({
+//          name:"Rahul Kumar"
+//     });
+//     // now i will stor the orders for cust1.. first i have to extract the orders for order table..
+//     let order1=await Order.findOne({item:"Chips"});
+//     let order2=await Order.findOne({item:"Chocolate"});
 
-    cust1.orders.push(order1);
-    cust1.orders.push(order2);
-  /// though we pushed order1 and order2 totally.. but in mongodb only there ID is pushed as in schema we defined type to be oBJECTID
-    let result=await cust1.save();
-    console.log(result);
-}
+//     cust1.orders.push(order1);
+//     cust1.orders.push(order2);
+//   /// though we pushed order1 and order2 totally.. but in mongodb only there ID is pushed as in schema we defined type to be oBJECTID
+//     let result=await cust1.save();
+//     console.log(result);
+// }
 //addCustomer();
+
+//fucntions..!!
 
 const findCustomer=async()=>{
   let result=await Customer.find({}).populate("orders");
   console.log(result[0]);
 }
+
+
+const addCust=async()=>{
+    let newCust=new Customer({
+        name:"Karan Arjun",
+    });
+    let newOrder=new Order({
+        item:"Pizza",
+        price:  250,
+    });
+    
+    newCust.orders.push(newOrder);
+
+    await newOrder.save();
+    await newCust.save();
+
+    console.log("added new customer");
+} 
+
+
+const delCust=async()=>{
+    let data=await Customer.findByIdAndDelete();
+    console.log();
+}
+addCust();
+delCust();
+
+
+// now i want if i delete customer.. his orders will also get deleted from database
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //console.log(res);
 // const addOrders=async()=>{
